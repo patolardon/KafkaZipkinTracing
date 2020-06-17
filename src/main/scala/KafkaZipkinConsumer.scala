95493e94415cda97877106df18d4b8a2463646b7
pierre <pierrelouandre@gmail.com>
2020-06-17 20:34:56 +0200

    added tracing to console for consumer

--- src/main/scala/KafkaZipkinConsumer.scala
+++ src/main/scala/KafkaZipkinConsumer.scala
@@ -1,3 +1,4 @@
+import java.lang.System.out
 import java.util.Properties
 
 import brave.Tracing
@@ -27,16 +28,21 @@ object KafkaZipkinConsumer extends App{
   properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092")
   properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
   properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
-  properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "mygroup")
+  properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "mygroup1")
   properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
   //create producer
-  val kafkaConsumer = new KafkaConsumer(properties)
-  kafkaConsumer.subscribe(util.Arrays.asList("my-topic-test"))
+  val kafkaConsumer = new KafkaConsumer[String, String](properties)
+  val tracingConsumer = kafkaTracing.consumer(kafkaConsumer)
+  tracingConsumer.subscribe(util.Arrays.asList("my-topic-test"))
   while (true) {
-    val consumerRecords = kafkaConsumer.poll(100)
+    val consumerRecords = tracingConsumer.poll(100)
     import scala.collection.JavaConversions._
     for (record <- consumerRecords) {
-      println(record.value)
+      val span = kafkaTracing.nextSpan(record).name("print-hello").start()
+      span.annotate("starting printing")
+      println(String.format("Record: %s", record))
+      span.annotate("printing finished")
+      span.finish()
     }
   }
 }
